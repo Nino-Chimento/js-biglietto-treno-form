6b80421805b0d082416c8f58f64463654331b431
Nino Chimento <saninoc84@gmail.com>
2019-12-19 14:03:28 +0100

    correzione valore distanza

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@ buttonGenera.addEventListener( "click",
     var nomeInput = document.getElementById('nomeutente');
     var nome = nomeutente.value;
     console.log(nome);
+    var km = document.getElementById("distanza");
+    var distanzaViaggio = km.value;
+    console.log(distanzaViaggio);
   }
 )
